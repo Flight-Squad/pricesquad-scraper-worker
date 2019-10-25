abc7baf75cfca8d81cff9409373993d71097c397
Pujit Mehrotra <pujitm2009@gmail.com>
2019-10-25 18:00:19 -0400

    Separate batch handler logic into looped single handler

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as AwsConfig from "config/aws";
 import * as SQS from "aws-sdk/clients/sqs";
 import { scrape } from "scrape";
 import logger from "config/winston";
-import axios from 'axios';
+import axios from "axios";
 import { PRICESQUAD_API } from "config/pricesquad.api";
 type SQSMessage = SQS.Types.Message;
 
@@ -15,27 +15,7 @@ const app = Consumer.create({
   region: AwsConfig.Region,
   handleMessageBatch: async (messages: SQSMessage[]) => {
     for (let message of messages) {
-      const data = JSON.parse(message.Body);
-
-      // Commented out because this can/should be handled more succinctly and scalably
-      // if (process.env.NODE_ENV === 'debug' || process.env.NODE_ENV === 'development') {
-        logger.debug(`Received Request ${data.params.requestId}`);
-      // }
-
-      try {
-        const res = await scrape(data);
-        const postUrl = `${PRICESQUAD_API}/tripRequest`;
-        const postBody = {
-          provider: data.provider,
-          results: res,
-          requestId: data.params.requestId,
-        };
-        await axios.post(postUrl, postBody);
-        logger.info(`Processed Request ${data.params.requestId}`)
-        // logger.info(JSON.stringify({requestId: data.params.requestId, res: res,}));
-      } catch (e) {
-        logger.error(e.message);
-      }
+      await handleMessage(message);
     } // end for-loop
   } // end handleMessageBatch
 });
@@ -48,5 +28,29 @@ app.on("processing_error", err => {
   logger.error(err.message);
 });
 
-logger.info('Starting app...')
+logger.info("Starting app...");
 app.start();
+
+async function handleMessage(message: SQSMessage) {
+  const data = JSON.parse(message.Body);
+
+  // Commented out because this can/should be handled more succinctly and scalably
+  // if (process.env.NODE_ENV === 'debug' || process.env.NODE_ENV === 'development') {
+  logger.debug(`Received Request ${data.params.requestId}`);
+  // }
+
+  try {
+    const res = await scrape(data);
+    const postUrl = `${PRICESQUAD_API}/tripRequest`;
+    const postBody = {
+      provider: data.provider,
+      results: res,
+      requestId: data.params.requestId
+    };
+    await axios.post(postUrl, postBody);
+    logger.info(`Processed Request ${data.params.requestId}`);
+    // logger.info(JSON.stringify({requestId: data.params.requestId, res: res,}));
+  } catch (e) {
+    logger.error(e.message);
+  }
+}
