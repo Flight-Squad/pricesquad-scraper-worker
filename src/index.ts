139603e402453bafb6db37f2c4d9a3b33f2e0b90
Pujit Mehrotra <pujitm2009@gmail.com>
2019-10-25 18:30:15 -0400

    Add repolling every 700ms

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ const app = Consumer.create({
   queueUrl: AwsConfig.QueueUrl,
   region: AwsConfig.Region,
   handleMessage: handleMessage,
+  pollingWaitTimeMs: 700,
 });
 
 app.on("error", err => {
