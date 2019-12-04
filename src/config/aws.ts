4493fc41cc61b2570acd6cebeb2a00815828ba83
Pujit Mehrotra <pmehrotra2@babson.edu>
2019-12-04 11:52:36 +0000

    Add env.template and add aws sqs env vars

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -1,2 +1,2 @@
-export const Region = 'us-east-2';
-export const QueueUrl = 'https://sqs.us-east-2.amazonaws.com/339600565640/pricesquad-trip-requests-0';
+export const Region = process.env.AWS_QUEUE_REGION;
+export const QueueUrl = process.env.AWS_QUEUE;
