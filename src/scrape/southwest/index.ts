a911401f1ad007969658797f5350ceb5e89b0341
Ajit Mehrotra <ajitmehrotra2009@gmail.com>
2019-12-22 13:03:43 -0500

    Add Southwest Return Trips

--- src/scrape/southwest/index.ts
+++ src/scrape/southwest/index.ts
@@ -1,20 +1,25 @@
 import { IFlightSearchParams } from "data/flight/search/params";
 import { getHtml } from "scrape/southwest/html";
-import { getTripsFromHtml } from "scrape/southwest/scrape";
 import makeUrl from "scrape/southwest/url";
+import { getDepartingTrips, getReturningTrips } from "./scrape";
 
 export async function southwestFlights(params: IFlightSearchParams) {
   const processStartTime = process.hrtime();
   const url = await makeUrl(params);
   const html = await getHtml(url);
-  console.log(html);
-  const trips = await getTripsFromHtml(html);
-
-  const processEndTime = process.hrtime(processStartTime);
-
-  return {
-    time: processEndTime,
-    data: trips,
+  const departingTrips = await getDepartingTrips(html);
+  const resData: any = {
+    data: departingTrips,
     url: url
   };
+
+  if (params.isRoundTrip && params.returnDate) {
+    const returningTrips = await getReturningTrips(html);
+    resData.returnTrips = returningTrips;
+  }
+
+  const processEndTime = process.hrtime(processStartTime);
+  resData.time = processEndTime;
+
+  return resData;
 }
--- src/scrape/southwest/scrape.ts
+++ src/scrape/southwest/scrape.ts
@@ -1,66 +1,91 @@
 import cheerio from "cheerio";
 import currencyFormatter from "currency-formatter";
 
-export async function getTripsFromHtml(html) {
+/**
+ *
+ * @param html The html to parse
+ */
+export async function getDepartingTrips(html) {
   const trips = [];
   const scraper = cheerio.load(html);
-  //Not 100% sure what the thing below does
 
-  //Should I get the span since the ul doesn't have a class really
+  scraper(".air-booking-select-price-matrix")
+    .filter(function(i, el) {
+      return scraper(this).attr("id") === "air-booking-product-0";
+    })
+    .each((i, elem) => parseTrips(i, elem, trips));
+
+  console.log(trips.length);
+  return trips;
+}
+
+/**
+ *
+ * @param html The html to parse
+ */
+export async function getReturningTrips(html) {
+  const trips = [];
+  const scraper = cheerio.load(html);
+
+  scraper(".air-booking-select-price-matrix")
+    .filter(function(i, el) {
+      return scraper(this).attr("id") === "air-booking-product-1";
+    })
+    .each((i, elem) => parseTrips(i, elem, trips));
+
+  return trips;
+}
+
+/**
+ *
+ * @param i
+ * @param elem
+ * @param trips Array to push the trip obejects into
+ */
+function parseTrips(i, elem, trips) {
+  const scraper = cheerio.load(elem);
   scraper("ul").each(function(i, elem) {
     scraper(this)
       .find("li")
       .each(function(i, elem) {
         const trip: any = { times: {} };
 
-        trip.times.depart = scraper(this) //Can't get this due to the type='origination' or type='destination'
-          .find(".air-operations-time-status") //.find(".select-detail--time")
+        trip.times.depart = scraper(this)
+          .find(".air-operations-time-status")
           .filter(function(i, el) {
             return scraper(this).attr("type") === "origination";
           })
           .text()
           .trim();
 
-        trip.times.arrival = scraper(this) //Can't get this due to the type='origination' or type='destination'
-          .find(".air-operations-time-status") //.find(".select-detail--time")
+        trip.times.arrival = scraper(this)
+          .find(".air-operations-time-status")
           .filter(function(i, el) {
             return scraper(this).attr("type") === "destination";
           })
           .text()
           .trim();
-        console.log(`Times: ${trip.times}`);
+
         trip.duration = scraper(this)
           .find(".flight-stops--duration-time")
           .text()
           .trim();
-        console.log(`Duration: ${trip.duration}`);
+
         trip.stops = scraper(this)
           .find(".flight-stops-badge")
           .text()
           .trim();
-        console.log(`Stops: ${trip.stops}`);
-        // results in something like
-        // "$299         $299"
+
         trip.price = scraper(this)
-          .find(".fare-button--value-total") //This prolly won't work bc it would list out all of them. And div layout doesn't make sense.
+          .find(".fare-button--value-total")
           .last()
           .text();
-        console.log(`Price: ${trip.price}`);
 
         trip.price = Number(trip.price);
-
-        // naiive
-        // price = price.split(" ")[0].trim();
-        // console.log(price);
-
-        // trip.price = currencyFormatter.unformat(price, { code: "USD" });
 
         if (trip.price) {
           trips.push(trip);
         }
       });
   });
-  // Sorting takes unnecessary cpu time, this can be done client-side/ on a database call
-  // trips.sort((a,b) => a.price - b.price);
-  return trips;
 }
