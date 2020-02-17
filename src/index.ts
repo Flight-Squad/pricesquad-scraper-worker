b09ea7c619ad96e9d52f131cbc09dea8d5f9dc8c
Pujit Mehrotra <pujitm2009@gmail.com>
2020-02-17 19:17:48 +0000

    Fix #8 #4pcn50[review]

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,18 @@ enum ExitCodes {
 }
 
 async function main(): Promise<void> {
-    const args = process.argv.slice(2);
-    console.log('Program Args:', args);
-
-    const query = JSON.parse(args[0]);
-    console.log('Query', query);
-
     try {
+        const args = process.argv.slice(2);
+        console.log('Program Args:', args);
+
+        const query = JSON.parse(args[0]);
+        console.log('Query', query);
+
         await onMessage(query);
         process.exit(ExitCodes.Success);
     } catch (e) {
+        // See https://github.com/Flight-Squad/pricesquad-scraper-worker/issues/8
+        // Use try-catch to force process to exit with non-zero code on uncaught exception
         console.error(e.name);
         console.error(e.message);
         console.error(e.stack);
